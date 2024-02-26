01d1ce9939b6407ce2f7abc7dfb3ae5ba4050169
Adauto Benevides Couto <daucouto9@gmail.com>
2024-02-26 20:17:44 -0300

    finalizações deixando as paginas dinâmicas

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -8,15 +10,34 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ContentComponent implements OnInit {
 
   @Input()
-  photoCover: string = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e737d025-51b8-4b27-93ad-e98aa14f2fb5/d2vfetm-024b469f-90fe-4405-afc2-901851bfe3da.jpg/v1/fill/w_1192,h_670,q_70,strp/naruto_sennin_mode_wallpaper_by_klarisag_d2vfetm-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZTczN2QwMjUtNTFiOC00YjI3LTkzYWQtZTk4YWExNGYyZmI1XC9kMnZmZXRtLTAyNGI0NjlmLTkwZmUtNDQwNS1hZmMyLTkwMTg1MWJmZTNkYS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.rxaUbZoKD8CinLa9faENyKvSrqPb7hXGOb9xyW56Hbs"
+  photoCover: string = ""
   @Input()
-  contentTitle: string = "MINHA NOTICIA"
+  contentTitle: string = ""
   @Input()
-  contentDescription: string = "Olá mundo"
+  contentDescription: string = ""
 
-  constructor() { }
+  private id: string | null = "0"
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponet(this.id)
   }
 
+  setValuesToComponet(id: string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+
+    if(!result){
+
+    }
+  }
 }
--- src/data/dataFake.ts
+++ src/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": 'ahhhhhh',
-    "title": "saiu novo filme",
-    "description": "ola",
+    "id": "1",
+    "title": "Novo Naruto é lançado nos EUA",
+    "description": "Novo filme do naruto está previsto para ser lançado em maio, sendo desenvolvido nos EUA",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8-hI-zXwEi8TJU9EPT7ytjpCbDrbOVHE_0EEybsIHCWwIoLHU9bqw7l9RXXos7HTFyks&usqp=CAU",
   },
   {
-    "id": 2,
-    "photo": 'ahhhhhh',
-    "title": "saiu novo filme",
-    "description": "ola",
+    "id": "2",
+    "title": "Narutinho vem ai",
+    "description": "Naruto filme",
+    "photoCover": "https://images5.alphacoders.com/841/841139.jpg",
+  },
+  {
+    "id": "3",
+    "title": "Rock Lee o filme",
+    "description": "Rock Lee filme",
+    "photoCover": "https://cdn.wallpapersafari.com/45/70/IBuDVF.jpg",
+  },
+  {
+    "id": "4",
+    "title": "Novo Naruto já está nos cinemas",
+    "description": "Naruto filme novo lançamento",
+    "photoCover": "https://wallpapers-clan.com/wp-content/uploads/2023/11/naruto-orange-minimalist-desktop-wallpaper-preview.jpg",
   },
 ]
